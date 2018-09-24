7686637f7e8527b2c90b716dde085b735834867a
lab2 <thagana44@gmail.com>
2018-09-24 15:04:26 +0300

    classification of text

---
+++ basics.py
@@ -0,0 +1,16 @@
+import nltk
+from nltk import sent_tokenize,word_tokenize,wordpunct_tokenize,ne_chunk,pos_tag
+#corpora-body of text eg speech
+#lexicon-words and meanings
+sample='Hello world. This is a sample text by Kimaru Thagana'
+tokenized_sentence=sent_tokenize(sample) # extract sentences from a piece of text
+tokenized_words=word_tokenize(sample) # extract words from a piece of text
+words=wordpunct_tokenize(sample) #also seperates out punctuations
+print(tokenized_sentence)
+print(tokenized_words)
+print(words)
+print(nltk.pos_tag(tokenized_words))# parts of speech tagging
+def entities(text):
+    return ne_chunk((pos_tag(word_tokenize(text) ) ) )
+comment=entities("When asked about the comments, Obama told the BBC: 'The UK will not be able to negotiate something with the US;")
+comment.pprint()
---
+++ text_classification.py
@@ -0,0 +1,31 @@
+import nltk,random
+from nltk.corpus import movie_reviews
+
+documents=[(list(movie_reviews.words(fileid)), category) #tuple containing a word and its category, whether +ve oe -ve
+           for category in movie_reviews.categories()
+           for fileid in movie_reviews.fileids(category)]
+
+random.shuffle(documents)
+all_words=[]
+for word in movie_reviews.words():
+    all_words.append(word.lower())
+
+all_words=nltk.FreqDist(all_words)
+print(all_words.most_common(5)) # top 5 words with the highest frequency
+print(all_words['love']) # how many times the word love appears
+
+word_features=list(all_words.keys())[:3000]
+
+def find_features(document):
+    words=set(document)
+    features={}
+    for w in word_features:
+        features[w]=(w in words)
+    return features
+
+#https://www.youtube.com/watch?v=-vVskDsHcVc
+#https://www.youtube.com/watch?v=5xDE06RRMFk
+
+#https://www.youtube.com/watch?v=6WpnxmmkYys
+# preparing datasets
+#https://www.youtube.com/watch?v=0xVqLJe9_CY
