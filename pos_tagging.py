c8a5b2d966902721a6194aa8c19499fc550a1388
lab2 <thagana44@gmail.com>
2018-11-08 14:06:39 +0300

    pos tagging on GW Bush script

---
+++ pos_tagging.py
@@ -0,0 +1,20 @@
+import nltk
+from nltk.corpus import state_union
+from nltk.tokenize import PunktSentenceTokenizer
+
+text=state_union.raw('2006- GWBush.txt')
+custom_tnzr=PunktSentenceTokenizer(text)# this is synonimous to training the tokenizer using the corpus provided
+tokenized=custom_tnzr.tokenize(text)
+
+def process_content():
+
+    try:
+        for i in tokenized:
+            words=nltk.word_tokenize(i)
+            tagged=nltk.pos_tag(words)
+            print(tagged)
+
+    except Exception as e:
+        print(str(e))
+
+process_content()
