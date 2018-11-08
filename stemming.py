7d99d15e0f267a4206604cd280e17a23c0800c26
lab2 <thagana44@gmail.com>
2018-11-08 13:38:12 +0300

    add side notes

--- stemming.py
+++ stemming.py
@@ -8,3 +8,5 @@ ps=PorterStemmer()
 example_words=['python','pythoner','pythoning','pythoned']
 for w in example_words:
     print(ps.stem(w))
+
+# use ps stemmer to stem a sentence after it being tokenized. Ensure the sentence has varied tense verbs
