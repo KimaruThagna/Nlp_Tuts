81dc472c0cbea2c31e6c00099689cb41cc8940e2
Kimaru Thagana <thagana44@gmail.com>
2018-11-11 13:01:47 -0300

    additional comments

--- stemming.py
+++ stemming.py
@@ -5,7 +5,7 @@ example of stemmers...brown stemmer, porter stemmer, snowball stemmer
 from nltk.stem import PorterStemmer,SnowballStemmer
 from nltk.tokenize import word_tokenize
 ps=PorterStemmer()
-example_words=['python','pythoner','pythoning','pythoned']
+example_words=['python','pythoner','pythoning','pythoned','going','went','go','delayed','coming','running']
 for w in example_words:
     print(ps.stem(w))
 
@@ -17,3 +17,4 @@ print('=================++++++=============++=\n Snowball stemmer')
 for w in example_words:
     print(sb.stem(w))
 # Comparison with lematization
+print('=================++++++=============++=\n Lematization')
