6852d36043baf0409c3c832d131491006fd6779a
Kimaru thagana <macbuntu@localhost.localdomain>
2019-09-18 20:47:57 +0300

    capitalization, intensifiers and punctuation

--- setimentAnalysis.py
+++ setimentAnalysis.py
@@ -8,3 +8,22 @@ def sentiment_analyzer_scores(sentence):
     print(f'{sentence} polarity scores: {score}')
 
 sentiment_analyzer_scores("This phone is super cool")
+
+# punctuation. ! shows a degree of intensity. This is good !!!! is more intense compared to this is good
+print(">>>>>>>>>>>>>>>>>Punctuation>>>>>>>>>>>>>>>>>")
+sentiment_analyzer_scores("This food is good")
+sentiment_analyzer_scores("This food is good!!")
+sentiment_analyzer_scores("This food is good!!!")
+
+# capitalization. Denotes emphasis
+print(">>>>>>>>>>>>>>>>>Capitalization>>>>>>>>>>>>>>>>>")
+sentiment_analyzer_scores("This food is good!")
+sentiment_analyzer_scores("This food is GREAT")
+sentiment_analyzer_scores("This food is great")
+
+
+# Degree  modifiers/intensifier. Impact intensity positively or negatively
+print(">>>>>>>>>>>>>>>>>Intensifiers>>>>>>>>>>>>>>>>>")
+sentiment_analyzer_scores("The lunch was extremely good")
+sentiment_analyzer_scores("The lunch was EXTREMELY bad")
+sentiment_analyzer_scores("The lunch was marginally good")
