c7aa2dfeee90f0c2c857fe72df97013278297c44
Kimaru thagana <macbuntu@localhost.localdomain>
2019-09-18 20:39:25 +0300

    scoring sentiment

--- setimentAnalysis.py
+++ setimentAnalysis.py
@@ -1,1 +1,10 @@
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+analyser = SentimentIntensityAnalyzer()
+
+def sentiment_analyzer_scores(sentence):
+    score = analyser.polarity_scores(sentence) # obtain polarity ndex of given sentence.
+    # produces the index of the -ve, +ve, neutral and compound sentiment
+    # the compound score is a sum of all lexicon ratings normalized between -1(very negative) to 1 (very positive)
+    print(f'{sentence} polarity scores: {score}')
+
+sentiment_analyzer_scores("This phone is super cool")
