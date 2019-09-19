eef211f5379cafbc84c69f70dad1b8ca36c03bb6
Kimaru thagana <macbuntu@localhost.localdomain>
2019-09-19 13:21:25 +0300

    data cleaning using regex

--- analytics_vidhya.py
+++ analytics_vidhya.py
@@ -21,3 +21,30 @@ print(train["label"].value_counts())
 sns.countplot(train["label"])
 plt.title("Label Distribution")
 plt.show()
+
+# check distribution in terms of length of tweets in the train and test set
+length_train = train['tweet'].str.len()
+length_test = test['tweet'].str.len()
+print(length_train)
+plt.hist(length_train, bins=20, label="train_tweets")
+plt.hist(length_test, bins=20, label="test_tweets")
+plt.legend()
+plt.show()
+
+# combine train and test for data processing
+combined = train.append(test, ignore_index=True)
+print(combined.shape)
+
+# data cleaning function to remove any pattern
+def remove_pattern(input_txt, pattern):
+    r = re.findall(pattern, input_txt)
+    for i in r:
+        input_txt = re.sub(i, '', input_txt) # substitute pattern with blank
+    return input_txt
+
+'''
+Tweet cleaning steps
+1. Remove twitter handles since they dont add to the sentiment of the tweet(denoted by @user due to privacy)
+2. Remove punctuation numbers and special characters
+3. Normalize text data. eg stemming
+'''
