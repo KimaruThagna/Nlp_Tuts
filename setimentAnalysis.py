d7209a3ec375e1744d60688de7800ce36bd176ed
Kimaru thagana <macbuntu@localhost.localdomain>
2019-09-19 08:46:33 +0300

    analyze sentiment in emoticons and emojis

--- setimentAnalysis.py
+++ setimentAnalysis.py
@@ -2,7 +2,7 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 analyser = SentimentIntensityAnalyzer()
 
 def sentiment_analyzer_scores(sentence):
-    score = analyser.polarity_scores(sentence) # obtain polarity ndex of given sentence.
+    score = analyser.polarity_scores(sentence) # obtain polarity index of given sentence.
     # produces the index of the -ve, +ve, neutral and compound sentiment
     # the compound score is a sum of all lexicon ratings normalized between -1(very negative) to 1 (very positive)
     print(f'{sentence} polarity scores: {score}')
@@ -27,3 +27,37 @@ print(">>>>>>>>>>>>>>>>>Intensifiers>>>>>>>>>>>>>>>>>")
 sentiment_analyzer_scores("The lunch was extremely good")
 sentiment_analyzer_scores("The lunch was EXTREMELY bad")
 sentiment_analyzer_scores("The lunch was marginally good")
+
+
+#Conjunctions. They denote a shift in sentiment. eg but
+print(">>>>>>>>>>>>>>>>>Intensifiers>>>>>>>>>>>>>>>>>")
+sentiment_analyzer_scores("The lunch was extremely good")
+sentiment_analyzer_scores("The lunch was EXTREMELY bad")
+sentiment_analyzer_scores("The lunch was marginally good")
+
+
+# preceding Tri-gram
+print(">>>>>>>>>>>>>>>>>Intensifiers>>>>>>>>>>>>>>>>>")
+sentiment_analyzer_scores("The lunch was extremely good")
+sentiment_analyzer_scores("The lunch was EXTREMELY bad")
+sentiment_analyzer_scores("The lunch was marginally good")
+
+
+# Emojis
+print(">>>>>>>>>>>>>>>>>Emoji>>>>>>>>>>>>>>>>>")
+sentiment_analyzer_scores("I am 😄 today")
+sentiment_analyzer_scores("😊")
+sentiment_analyzer_scores("😥")
+print(sentiment_analyzer_scores('☹️'))
+
+
+# Common slang
+print(">>>>>>>>>>>>>>>>>Slang>>>>>>>>>>>>>>>>>")
+print(sentiment_analyzer_scores("Today SUX!"))
+print(sentiment_analyzer_scores("Today only kinda sux! But I'll get by, lol"))
+
+
+# Emoticon
+print(">>>>>>>>>>>>>>>>>Emoticons>>>>>>>>>>>>>>>>>")
+sentiment_analyzer_scores("You either feel :) or :D")
+sentiment_analyzer_scores("wow. That was :(")
