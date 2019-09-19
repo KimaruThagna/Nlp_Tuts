3625ff4f6c0d73195b692e14bba620a933e8a48a
Kimaru thagana <macbuntu@localhost.localdomain>
2019-09-19 11:26:34 +0300

    inspect and visualize label distribution

--- analytics_vidhya.py
+++ analytics_vidhya.py
@@ -7,6 +7,17 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-
+# racist/ sexist tweets are labelled with 1 else, 0
 train  = pd.read_csv('datasets/train_E6oV3lV.csv')
 test = pd.read_csv('datasets/test_tweets_anuFYb8.csv')
+train.set_index("id")
+# check out some tweets from both classes
+print(f' NON RACIST?SEXIST Tweets>>>>{train[train["label"] == 0].head()}')
+print(f' Racist/sexist tweets {train[train["label"] == 1].head()}')
+# inspect label distribution
+print(train["label"].value_counts())
+
+# visualize label distribution
+sns.countplot(train["label"])
+plt.title("Label Distribution")
+plt.show()
