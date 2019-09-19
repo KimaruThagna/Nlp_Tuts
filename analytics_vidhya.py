77360709d1db85a41dc8513eeb18ab6c12d798d8
Kimaru thagana <macbuntu@localhost.localdomain>
2019-09-19 10:26:47 +0300

    analytics vidhya course add file and relevant libraries

---
+++ analytics_vidhya.py
@@ -0,0 +1,12 @@
+import re    # for regular expressions
+import nltk  # for text manipulation
+import string
+import warnings
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+train  = pd.read_csv('train_E6oV3lV.csv')
+test = pd.read_csv('test_tweets_anuFYb8.csv')
