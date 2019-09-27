485e6074860483fa2ef7c6018549924041eea9c9
Kimaru thagana <macbuntu@localhost.localdomain>
2019-09-27 15:06:35 +0300

    SVM classifier

---
+++ svm_classifier.py
@@ -0,0 +1,4 @@
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score
+from analytics_vidhya import bow,train, np, test,tfidf,wordvec_df, docvec_df
