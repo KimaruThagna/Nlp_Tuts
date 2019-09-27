4cbc33aa44bd821979ae8de8dd78850bb8a50944
Kimaru thagana <macbuntu@localhost.localdomain>
2019-09-27 13:56:09 +0300

    logistic regression on TF-IDF features

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,13 +1,14 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score
-from analytics_vidhya import bow,train, np
+from analytics_vidhya import bow,train, np, test,tfidf
+lreg = LogisticRegression()
 # Extracting train and test BoW features
 train_bow = bow[:31962,:]
 test_bow = bow[31962:,:]
 # splitting data into training and validation set
-xtrain_bow, xvalid_bow, ytrain, yvalid = train_test_split(train_bow, train['label'],                                                            random_state=42,                                                           test_size=0.3)
-lreg = LogisticRegression()
+xtrain_bow, xvalid_bow, ytrain, yvalid = train_test_split(train_bow, train['label'], random_state=42, test_size=0.3)
+
 # training the model
 lreg.fit(xtrain_bow, ytrain)
 prediction = lreg.predict_proba(xvalid_bow) # predicting on the validation set
@@ -16,3 +17,23 @@ prediction_int = prediction_int.astype(np.int)
 score_f1 = f1_score(yvalid, prediction_int) # calculating f1 score for the validation set
 
 print(f'F1 score for logistic regression on the BOW features{score_f1}')
+
+#Now let’s make predictions for the test dataset and create a submission file.
+
+test_pred = lreg.predict_proba(test_bow)
+test_pred_int = test_pred[:,1] >= 0.3
+test_pred_int = test_pred_int.astype(np.int)
+test['label'] = test_pred_int
+submission = test[['id','label']]
+submission.to_csv('sub_lreg_bow.csv', index=False) # writing data to a CSV file
+
+# tf-idf featureset
+train_tfidf = tfidf[:31962,:]
+test_tfidf = tfidf[31962:,:]
+xtrain_tfidf = train_tfidf[ytrain.index]
+xvalid_tfidf = train_tfidf[yvalid.index]
+lreg.fit(xtrain_tfidf, ytrain)
+prediction = lreg.predict_proba(xvalid_tfidf)
+prediction_int = prediction[:,1] >= 0.3
+prediction_int = prediction_int.astype(np.int)
+f1_score(yvalid, prediction_int) # calculating f1 score for the validation set
