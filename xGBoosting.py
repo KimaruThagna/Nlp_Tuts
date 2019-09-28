8162ad76c633ca2518312cb9ca980ec219939765
Kimaru thagana <macbuntu@localhost.localdomain>
2019-09-28 16:19:51 +0300

    XGBoosting classifier parameter tuning

--- xGBoosting.py
+++ xGBoosting.py
@@ -1,4 +1,5 @@
 from xgboost import XGBClassifier
+import xgboost as xgb
 from logistic_regression import *
 
 # BOW features
@@ -26,3 +27,66 @@ print(f'F1 score for XGBoost Word2Vec features {f1_score(yvalid, prediction)}')
 xgb = XGBClassifier(max_depth=6, n_estimators=1000, nthread= 3).fit(xtrain_d2v, ytrain)
 prediction = xgb.predict(xvalid_d2v)
 print(f'F1 score for XGBoost Doc2Vec features {f1_score(yvalid, prediction)}')
+
+
+'''
+XGBOOST + WORD2VEC  PARAMETER TUNING
+'''
+#using D matrices. Can hold both features and target variable
+dtrain = xgb.DMatrix(xtrain_w2v, label=ytrain)
+dvalid = xgb.DMatrix(xvalid_w2v, label=yvalid)
+dtest = xgb.DMatrix(test_w2v)
+# Parameters that we are going to tune
+params = {
+    'objective':'binary:logistic',
+    'max_depth':6,
+    'min_child_weight': 1,
+    'eta':.3,
+    'subsample': 1,
+    'colsample_bytree': 1
+ }
+
+# define evaluation metric
+def custom_eval(preds, dtrain):
+    labels = dtrain.get_label().astype(np.int)
+    preds = (preds >= 0.3).astype(np.int)
+    return [('f1_score', f1_score(labels, preds))]
+
+
+#Tuning max_depth and min_child_weight
+
+gridsearch_params = [
+    (max_depth, min_child_weight)
+    for max_depth in range(6, 10)
+    for min_child_weight in range(5, 8)
+]
+max_f1 = 0.  # initializing with 0
+best_params = None
+for max_depth, min_child_weight in gridsearch_params:
+    print("CV with max_depth={}, min_child_weight={}".format(
+        max_depth,
+        min_child_weight))
+    # Update our parameters
+    params['max_depth'] = max_depth
+    params['min_child_weight'] = min_child_weight
+
+    # Cross-validation
+    cv_results = xgb.cv(params,
+                        dtrain, feval=custom_eval,
+                        num_boost_round=200,
+                        maximize=True,
+                        seed=16,
+                        nfold=5,
+                        early_stopping_rounds=10
+                        )
+# Finding best F1 Score
+
+mean_f1 = cv_results['test-f1_score-mean'].max()
+
+boost_rounds = cv_results['test-f1_score-mean'].argmax()
+print("\tF1 Score {} for {} rounds".format(mean_f1, boost_rounds))
+if mean_f1 > max_f1:
+    max_f1 = mean_f1
+    best_params = (max_depth, min_child_weight)
+
+print("Best params: {}, {}, F1 Score: {}".format(best_params[0], best_params[1], max_f1))
