bf7cfeabe1a0f03210c54ed835c615fa2109e0a5
Kimaru thagana <macbuntu@localhost.localdomain>
2019-09-28 15:17:48 +0300

    svm classifier on TF-IDF fetures

--- svm_classifier.py
+++ svm_classifier.py
@@ -1,7 +1,6 @@
 from sklearn import svm
 from sklearn.metrics import f1_score
-from logistic_regression import xtrain_bow, ytrain, xvalid_bow, test_bow, yvalid, np, test
-
+from logistic_regression import *
 svc = svm.SVC(kernel='linear', C=1, probability=True).fit(xtrain_bow, ytrain)
 prediction = svc.predict_proba(xvalid_bow)
 prediction_int = prediction[:,1] >= 0.3
@@ -14,3 +13,11 @@ test_pred_int = test_pred_int.astype(np.int)
 test['label'] = test_pred_int
 submission = test[['id','label']]
 submission.to_csv('sub_svm_bow.csv', index=False)
+
+# using TF-IDF features
+svc = svm.SVC(kernel='linear',
+C=1, probability=True).fit(xtrain_tfidf, ytrain)
+prediction = svc.predict_proba(xvalid_tfidf)
+prediction_int = prediction[:,1] >= 0.3
+prediction_int = prediction_int.astype(np.int)
+print(f'F1 score for SVM TF-IDF features {f1_score(yvalid, prediction_int)}')
