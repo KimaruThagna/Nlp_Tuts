3ba334b873a6c619e31b4408da8e95006df29f59
Kimaru thagana <macbuntu@localhost.localdomain>
2019-09-28 15:28:05 +0300

    random forest classifier with TF-IDF features

--- random_forests_classifier.py
+++ random_forests_classifier.py
@@ -3,9 +3,16 @@ from sklearn.ensemble import RandomForestClassifier
 rf = RandomForestClassifier(n_estimators=400, random_state=11).fit(xtrain_bow, ytrain)
 prediction = rf.predict(xvalid_bow)
 # validation score
-f1_score(yvalid, prediction)
+print(f'F1 score for RandomForest BOW features {f1_score(yvalid, prediction)}')
+
 # create submission file
 test_pred = rf.predict(test_bow)
 test['label'] = test_pred
 submission = test[['id','label']]
 submission.to_csv('sub_rf_bow.csv', index=False)
+
+# predict o TF-IDF features
+rf = RandomForestClassifier(n_estimators=400, random_state=11).fit(xtrain_tfidf, ytrain)
+prediction = rf.predict(xvalid_tfidf)
+print(f'F1 score for RandomForest BOW features {f1_score(yvalid, prediction)}')
+
