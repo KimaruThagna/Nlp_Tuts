92160202c0af133d484c075c4307ccde740f8b33
Kimaru thagana <macbuntu@localhost.localdomain>
2019-10-22 20:49:20 +0300

    create column category

--- multi-class-text-classification.py
+++ multi-class-text-classification.py
@@ -2,3 +2,18 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
+import pandas as pd
+from io import StringIO
+df = pd.read_csv('datasets/Consumer_Complaints.csv')
+print(f'Consumer complaints dataset view\n{df.head()}')
+
+relevant_col = ['Product', 'Consumer complaint narrative']
+df = df[relevant_col]
+
+df = df[pd.notnull(df['Consumer complaint narrative'])]
+df.columns = ['Product', 'Consumer_complaint_narrative']
+df['category_id'] = df['Product'].factorize()[0]
+category_id_df = df[['Product', 'category_id']].drop_duplicates().sort_values('category_id')
+category_to_id = dict(category_id_df.values)
+id_to_category = dict(category_id_df[['category_id', 'Product']].values)
+print(f'Consumer complaints {df.head()}')
