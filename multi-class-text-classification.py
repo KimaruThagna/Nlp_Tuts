ae9e5c52410fd68c2c7d0be02d5754ca422f5cb0
Kimaru thagana <macbuntu@localhost.localdomain>
2019-10-24 20:22:54 +0300

    viewing false positives and false negatives

--- multi-class-text-classification.py
+++ multi-class-text-classification.py
@@ -4,11 +4,16 @@ from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_selection import chi2
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import LinearSVC
+from sklearn.model_selection import cross_val_score
 import pandas as pd
-from io import StringIO
+from sklearn.metrics import confusion_matrix
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
+
 df = pd.read_csv('datasets/Consumer_Complaints.csv')
 #print(f'Consumer complaints dataset view\n{df.head()}')
 
@@ -58,3 +63,49 @@ clf = MultinomialNB().fit(X_train_tfidf, y_train)
 print(clf.predict(count_vect.transform(["This company refuses to provide me verification and validation of debt per my right under the FDCPA. I do not believe this debt is mine."])))
 #Test this out
 print(df[df['Consumer_complaint_narrative'] == "This company refuses to provide me verification and validation of debt per my right under the FDCPA. I do not believe this debt is mine."])
+
+# test different models
+models = [
+    RandomForestClassifier(n_estimators=200, max_depth=3, random_state=0),
+    LinearSVC(),
+    MultinomialNB(),
+    LogisticRegression(random_state=0),
+]
+CV = 5
+cv_df = pd.DataFrame(index=range(CV * len(models)))
+entries = []
+for model in models:
+  model_name = model.__class__.__name__
+  accuracies = cross_val_score(model, features, labels, scoring='accuracy', cv=CV)
+  for fold_idx, accuracy in enumerate(accuracies):
+    entries.append((model_name, fold_idx, accuracy))
+cv_df = pd.DataFrame(entries, columns=['model_name', 'fold_idx', 'accuracy'])
+
+sns.boxplot(x='model_name', y='accuracy', data=cv_df)
+sns.stripplot(x='model_name', y='accuracy', data=cv_df,
+              size=8, jitter=True, edgecolor="gray", linewidth=2)
+plt.show()
+print(f'CV DATAFRAME {cv_df}')
+print(cv_df.groupby('model_name').accuracy.mean())
+# choose best model, LinearSVC
+
+model = LinearSVC()
+X_train, X_test, y_train, y_test, indices_train, indices_test = train_test_split(features, labels, df.index, test_size=0.33, random_state=0)
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+conf_mat = confusion_matrix(y_test, y_pred)
+fig, ax = plt.subplots(figsize=(10,10))
+sns.heatmap(conf_mat, annot=True, fmt='d',
+            xticklabels=category_id_df.Product.values, yticklabels=category_id_df.Product.values)
+plt.ylabel('Actual')
+plt.xlabel('Predicted')
+plt.show()
+
+# viewing false positives and negatives
+for predicted in category_id_df.category_id:
+  for actual in category_id_df.category_id:
+    if predicted != actual and conf_mat[actual, predicted] >= 10:
+      print("'{}' predicted as '{}' : {} examples.".format(id_to_category[actual], id_to_category[predicted], conf_mat[actual, predicted]))
+      print(df.loc[indices_test[(y_test == actual) & (y_pred == predicted)]][['Product', 'Consumer_complaint_narrative']])
+      print('')
