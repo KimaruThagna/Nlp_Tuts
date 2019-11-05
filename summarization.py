ffef7035b9254f08ff06b7c59e835559453cdd6a
Kimaru thagana <macbuntu@localhost.localdomain>
2019-11-05 22:19:38 +0300

    text summarization

--- summarization.py
+++ summarization.py
@@ -1,2 +1,10 @@
 from gensim.summarization.summarizer import summarize
+from gensim.test.utils import common_dictionary, common_corpus
+from gensim.models import LsiModel
+#document summary
+model = LsiModel(common_corpus, id2word=common_dictionary)
+vectorized_corpus = model[common_corpus]
+
+text = 'Today was a good day. Started off with a good morning run, went out for a jog and later, when i came back,' \
+       'I went to work feeling fresh'
 print(summarize(text))
